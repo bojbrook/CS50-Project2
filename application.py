10f9bdf0bb784362defe9064626013d92457939d
Bowen Brooks <bowenjbrooks@gmail.com>
2018-09-30 18:49:14 -0700

    adding username page

--- application.py
+++ application.py
@@ -1,21 +1,38 @@
 import os
 import requests
 
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, session
+from flask_session import Session
 from flask_socketio import SocketIO, emit
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if not session.get('user'):
+        return render_template("login.html")
+    return render_template("index.html",user=session['user'])
+
+@app.route("/login", methods=["GET"])
+def login():
+    user_name = request.form.get('input-username')
+    print (user_name)
+    session['user'] = user_name
+    return render_template("index.html",user=user_name)
 
 
 @socketio.on("submit message")
 def message(data):
-    messages = data['message']
-    print (f"Here is the data {messages}")
-    emit("message sent", messages, broadcast=True)
+    messages = data['message'] 
+    print (f"Here is the data {data}")
+    emit("message sent", data, broadcast=True)
--- static/index.js
+++ static/index.js
@@ -1,16 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  // setting up the form submit for username
+  document.querySelector('form').onsubmit = () => {
+    console.log("Form was clicked");
+    var username = document.querySelector('#input-username').value;
+    console.log(username);
+    return false;
+  }
+
+
   // Connect to websocket
   var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
   // When connected, configure buttons
   socket.on('connect', () => {
+    
       // Each button should emit a "submit vote" event
-      document.querySelector('button').onclick = () => {
-        console.log('clicked button');
-        const message = document.querySelector('input').value;
+      document.querySelector('#button-message-send').onclick = () => {
+        const message = document.querySelector('#input-message-section').value;
+        const user = "Bowen Brooks";
         console.log(message);
-        socket.emit('submit message', {'message': message});
+        data = {
+          'message': message, 
+          'user': user
+        };
+        socket.emit('submit message', data);
       }; 
               
     });
@@ -18,7 +32,10 @@ document.addEventListener('DOMContentLoaded', () => {
   // // When a new vote is announced, add to the unordered list
   socket.on('message sent', data => {
     const li = document.createElement('li');
-    li.innerHTML = data;
+    const p = document.createElement('p');
+    p.innerHTML = data['user'];
+    li.innerHTML = data['message'];
+    li.appendChild(p);
     document.querySelector('#list').append(li);
   });
 
