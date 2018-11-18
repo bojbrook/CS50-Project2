1fd0905dede59e720e7fb1dc3c8fb6ad7802e5bf
Bowen Brooks <bowenjbrooks@gmail.com>
2018-11-18 12:25:14 -0800

    fixed the ID problem with the python classes

--- Channel.py
+++ Channel.py
@@ -3,15 +3,15 @@ from message import Message
 class Channel:
   def __init__(self,name):
     self.channel = name
-    self.messages = [100]
-    self.id = 0
+    self.messages = [] 
+    self.Id = 0
 
   def get_ChannelName(self):
     return self.channel
 
-  def add_message(self, user, message, time):
-      self.id += 1
-      m = Message(user,message,time,self.id)
+  def add_message(self,user, message,time):
+      self.Id += 1
+      m = Message(user,message,time,self.Id)
       self.messages.append(m)
   
   def get_messages(self):
--- message.py
+++ message.py
@@ -1,9 +1,9 @@
 class Message:
-  def __init__(self,user,message,time,id):
+  def __init__(self,user,message,time,ID):
     self.user = user
     self.message = message
     self.time = time
-    self.id = id
+    self.Id = ID
 
   def get_user(self):
     return self.user
@@ -14,8 +14,8 @@ class Message:
   def get_time(self):
     return self.time
 
-  def get_id(self):
-    return self.id
+  def get_ID(self):
+    return self.Id
 
   def toString(self):
-    return (f"ID: {self.id} User: {self.user} Message: {self.message} Time: {self.time} ")
+    return (f"ID: {self.Id} User: {self.user} Message: {self.message} Time: {self.time} ")
